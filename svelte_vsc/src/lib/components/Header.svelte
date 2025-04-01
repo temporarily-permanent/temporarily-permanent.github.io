633e64311dbf6f736f979e6917611462e290c7ed
Jannes <37171@ma-web.nl>
2025-04-01 09:50:33 +0200

    asdf

--- svelte_vsc/src/lib/components/Header.svelte
+++ svelte_vsc/src/lib/components/Header.svelte
@@ -3,17 +3,10 @@
 </script>
 
 <header>
-<a>
-	<img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Fjords" style="width:100%">
-	<h1>just some text</h1>
+	<a href="Header.svelte">
+		About Me
 	</a>
-	<a href="https://www.w3schools.com/w3images/fjords.jpg">
-	<img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Fjords" style="width:100%">
-	<h1>just some text</h1>
-	</a>
-	<a href="https://www.w3schools.com/w3images/fjords.jpg">
-	<img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Fjords" style="width:100%">
-</a>	<h1>just some text</h1>>
+	<h1>just some text</h1>>
 </header>
 
 <style>
