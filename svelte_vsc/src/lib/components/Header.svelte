d63155e0a89f6c50e0df72d259327cd0b6015206
Jannes <37171@ma-web.nl>
2025-04-01 09:29:33 +0200

    thanks webstore

--- svelte_vsc/src/lib/components/Header.svelte
+++ svelte_vsc/src/lib/components/Header.svelte
@@ -3,7 +3,17 @@
 </script>
 
 <header>
+<a>
+	<img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Fjords" style="width:100%">
 	<h1>just some text</h1>
+	</a>
+	<a href="https://www.w3schools.com/w3images/fjords.jpg">
+	<img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Fjords" style="width:100%">
+	<h1>just some text</h1>
+	</a>
+	<a href="https://www.w3schools.com/w3images/fjords.jpg">
+	<img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Fjords" style="width:100%">
+</a>	<h1>just some text</h1>>
 </header>
 
 <style>
