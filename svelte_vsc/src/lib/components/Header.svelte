50bf1ee98040da62e92304b2fb1acd1f08afe445
Jannes <37171@ma-web.nl>
2025-04-02 10:08:17 +0200

    mostly decided on color palette, continue work on header
    this is a war crime

--- svelte_vsc/src/lib/components/Header.svelte
+++ svelte_vsc/src/lib/components/Header.svelte
@@ -1,16 +1,26 @@
 <script>
 
+	//let data = ["":"About Me", "Color Palette"];
 </script>
 
 <header>
-	<a href="Header.svelte">
+	<a href="AboutMe">
 		About Me
 	</a>
-	<h1>just some text</h1>>
+	<a href="ColorPalette">
+		color palette
+		</a>
+	<h1>just some text<br>aaa</h1>
 </header>
 
 <style>
     header{
-				position: relative;
+				position: fixed;
+        bottom: 0px;
+        left: 50%;
+        transform: translate(-50%, -5%);
+        outline: 2px solid black;
+				display: flex;
+				flex-direction: row;
 		}
 </style>
---
+++ svelte_vsc/src/routes/AboutMe/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.title = 'About Me';
+	});
+</script>
+
+<h1>Welcome to AboutMe</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
