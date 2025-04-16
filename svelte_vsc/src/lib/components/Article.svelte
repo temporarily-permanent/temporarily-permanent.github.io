17236d01bcc486bd339cba82781c53dbc3778655
Jannes <37171@ma-web.nl>
2025-04-16 13:45:44 +0200

    finished pitch page

---
+++ svelte_vsc/src/lib/components/Article.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let title = "";
+	export let content = "";
+	export let direction = "left";
+	export let image = null;
+
+	export let style = "";
+	export let titleStyle = "";
+	export let contentStyle = "";
+</script>
+
+<div class="article fade-slide-blur {direction}" {style}>
+	<div>
+		<h2 style={titleStyle}>{title}</h2>
+		<p style={contentStyle}>{content}</p>
+	</div>
+
+	{#if image}
+		<img alt="article pic" src={image}/>
+	{/if}
+</div>
+
+<style>
+    .article {
+        padding: 25px;
+        --direction: 0;
+    }
+
+    .article.left {
+        --direction: translateX(-50px);
+        border-left: 1px solid white;
+
+        & h2::first-letter {
+            margin-left: -0.05em;
+        }
+    }
+
+    .article.right {
+        --direction: translateX(50px);
+        border-right: 1px solid white;
+        text-align: end;
+    }
+
+    h2 {
+        margin: 0;
+        font-family: sans-serif;
+    }
+
+    p {
+        margin: 0;
+        font-weight: 350;
+        font-size: 1.2rem;
+    }
+
+    img {
+        height: 100%;
+    }
+</style>
--- svelte_vsc/src/routes/Pitch/+page.svelte
+++ svelte_vsc/src/routes/Pitch/+page.svelte
@@ -4,13 +4,19 @@
 	<h1>Jannes Veuger</h1>
 	<h2>Low Level Engineer</h2>
 	<h3>About Me</h3>
+	<p>
+		i'm JV, a 21 year old software developer and interested in the computationally low-level and the technically complex<br><br>
+		in my free time i like inline skating, playing vidja gaem and programming<br>
+	</p>
 	<h4>background</h4>
 	<p>
 		 <!--20 jaar oud, geboren in 2003<br>-->
 		<!--2 de jaars student computer science aan de universiteit van twente<br>-->
-		2de jaars software development student bij het mediacollege Amsterdam.<br>
-
-		daarvoor ben ik afgestudeerd bij altra college, hier heb ik mijn vwo diploma gehaald.<br>
+		<!--2de jaars software development student bij het mediacollege Amsterdam.<br>-->
+		2nd year software development student at the mediacollege Amsterdam<br>
+		before that i graduated from the Altra college, where i got my VWO diploma.<br>
+		<!--daarvoor ben ik afgestudeerd bij altra college, hier heb ik mijn vwo diploma gehaald.<br>-->
+		<!--daarvoor heb ik mijn vmbo diploma gehaald op het clusius college in alkmaar<br>-->
 	</p>
 	<h4>About Me</h4>
 
@@ -19,6 +25,7 @@
 	<!--<p>I'm a low level engineer with a passion for creating efficient and effective solutions. I have experience in various programming languages and technologies, and I'm always looking to learn more.</p>-->
 	<h3>projects</h3>
 	<h4>textBasedAdventureGameFramework</h4>
+	<a href="https://youtu.be/dQw4w9WgXcQ">learn more</a>
 	<div>
 		<p>
 			a program to which you give all storylines defined in a json file,<br>
@@ -26,6 +33,24 @@
 		</p>
 
 	 	<img src="Json.png"><p>recursively go through the storyline</p><img src="Recursion.png" alt="aaaa">
+		<a href="https://youtu.be/dQw4w9WgXcQ">learn more</a>
+	</div>
+	<h4>Game Sclice</h4>
+	<div>
+		<a href="https://github.com/Interis-mk/Game-Sclice">github</a>
+		<p>
+			group assignment for school where we got a 10 sec clip of hollow knight <br>
+			and had recreate as much as possible in about 8-9 weeks
+		</p>
+		<p>
+			in this project i was assigned to the product owner role<br>
+			which was the first i had a leader role<br>
+		</p>
+
+		<div style="display: inline;">
+			<img src="HollowKnight.png" alt="aaaa">
+			<img src="GithubPullRequests.png" alt="aaaa">
+			</div>
 	</div>
 
 
@@ -54,11 +79,11 @@
 	}
 
 	img {
-		width: 100%;
+		width: auto;
 	}
 	.filler {
 		width: 100%;
-		height: 100px;
+		height: 16vh;
 		background-color: #000;
 	}
 </style>
