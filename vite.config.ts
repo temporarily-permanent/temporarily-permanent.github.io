9599debf96f6da075f408ffe03ae4bdf096910a2
Jannes <37171@ma-web.nl>
2025-08-08 13:37:30 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [ sveltekit()]
+	plugins: [ sveltekit()],
+	base : '/portfolio',
 });
