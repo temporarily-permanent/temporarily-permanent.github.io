95367dfd7ff7159738990c8cff13f0e746978b42
Jannes <37171@ma-web.nl>
2025-10-02 21:49:40 +0200

    Updates

---
+++ app/immutable/chunks/CJ0npYk3.js
@@ -0,0 +1,1 @@
+import{a5 as a}from"./VtR58Rjb.js";a();
---
+++ app/immutable/nodes/8.C5UNZqiN.js
@@ -0,0 +1,7 @@
+import"../chunks/CWj6FrbW.js";import{p as _,f as v,a as g,h as c,b as d,c as k,d as $,i as x,a8 as M,ad as q,r as y,s as O}from"../chunks/VtR58Rjb.js";import{i as j}from"../chunks/DzrFDL_6.js";import{r as w}from"../chunks/BSusBN0d.js";import{b as z,t as m,e as A}from"../chunks/Dj2gB9Rn.js";var B=v('<div class="centered svelte-8aqkg4"><span class="svelte-8aqkg4">transitions!</span></div>'),C=v('<label><input type="checkbox"/> visible</label> <!>',1);function I(f,u){_(u,!0);let s=M(!0);function r(a,{duration:t}){return{duration:t,css:(n,l)=>{const p=A(n);return`
+					transform: scale(${p}) rotate(${p*3*1080}deg);
+					color: hsl(
+						${Math.trunc(n*360)},
+						${Math.min(100,1e3*l)}%,
+						${Math.min(50,500*l)}%
+					);`}}}var i=C(),e=g(i),o=$(e);w(o),q(),y(e);var b=O(e,2);{var h=a=>{var t=B();m(1,t,()=>r,()=>({duration:8e3})),m(2,t,()=>r,()=>({duration:8e3})),d(a,t)};j(b,a=>{c(s)&&a(h)})}z(o,()=>c(s),a=>x(s,a)),d(f,i),k()}export{I as component};
