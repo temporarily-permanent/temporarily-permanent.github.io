fa4a1b7fd495a9e29000c2a49354500529d77853
Jannes <37171@ma-web.nl>
2025-10-03 14:58:12 +0200

    Updates

--- app/immutable/entry/start.D3yAm5Wo.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BwzPkc9u.js";export{o as load_css,r as start};
---
+++ app/immutable/nodes/2.DiWOoJX7.js
@@ -0,0 +1,7 @@
+import"../chunks/CWj6FrbW.js";import{p as q,f as g,a as w,t as D,h as u,b,c as O,d as t,r as s,s as l,a8 as j,i as z}from"../chunks/VtR58Rjb.js";import{s as A}from"../chunks/CcJ6qbVl.js";import{i as B}from"../chunks/DzrFDL_6.js";import{r as C}from"../chunks/BSusBN0d.js";import{b as E,t as _,e as F}from"../chunks/Dj2gB9Rn.js";import{g as G}from"../chunks/Bs2I6pEL.js";var H=g('<div class="centered svelte-12gbqy"><span class="svelte-12gbqy">transitions!</span></div>'),I=g(`<h2 class="svelte-12gbqy"><a> </a></h2> <p class="text_centered">i'm a 22 year old software developer who is interested in the computationally low-level and the technically complex</p> <p class="text_centered">and i like to figure out mathematically complex problems --> <label><input type="checkbox"/></label></p> <!>`,1);function S(x,y){q(y,!0);let r=j(!1);function n(e,{duration:a}){return{duration:a,css:(v,f)=>{const h=F(v);return`
+					transform: scale(${h}) rotate(${h*3*1080}deg);
+					color: hsl(
+						${Math.trunc(v*360)},
+						${Math.min(100,1e3*f)}%,
+						${Math.min(50,500*f)}%
+					);`}}}var p=I(),o=w(p),c=t(o),$=t(c,!0);s(c),s(o);var i=l(o,4),m=l(t(i)),d=t(m);C(d),s(m),s(i);var k=l(i,2);{var M=e=>{var a=H();_(1,a,()=>n,()=>({duration:8e3})),_(2,a,()=>n,()=>({duration:8e3})),b(e,a)};B(k,e=>{u(r)&&e(M)})}D(()=>A($,G.psudo_name)),E(d,()=>u(r),e=>z(r,e)),b(x,p),O()}export{S as component};
