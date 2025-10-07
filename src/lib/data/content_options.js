3c141af3450e88d63d006d091195f25f726795a6
Jannes <37171@ma-web.nl>
2025-10-07 11:36:19 +0200

    ordering portfolio

--- src/lib/data/content_options.js
+++ src/lib/data/content_options.js
@@ -96,9 +96,9 @@ const portfolioData = {
 		content: [
 			{ type: 0, content: 'this portfolio website' },
 			{ type: 4, url:'https://github.com/temporarily-permanent/temporarily-permanent.github.io', content: 'GITHUB' },
-			{ type: 1, content: 'It has taken me upwards of 40 working hours to learn and build everything and was done over lots of little bite sized pieces of time to pace myself and burn out creatively.'},
 			{ type: 1, content: 'This website is built using SvelteKit and vite and is hosted on GitHub pages. '},
 			{ type: 1, content: 'Porkbun was used as the domain regstrar mainly because i liked their marketing. '},
+			{ type: 1, content: 'It has taken me upwards of 40 working hours to learn and build everything and was done over lots of little bite sized pieces of time to pace myself and burn out creatively.'},
 					//{'I built this website from scratch}, including the design and layout. I wanted to create a simple and clean website that showcases my work and skills.'},
 		]
 	}]
