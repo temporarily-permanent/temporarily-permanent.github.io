76eb722a0d685ace859cd73a33c2aa368ed0c34f
Jannes <37171@ma-web.nl>
2025-10-31 20:46:43 +0100

    seen enough, i'm satisfied

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
-    import {genericMetaData} from "$lib/data/content_options.js";
+    import {genericMetaData, portfolioData} from "$lib/data/content_options.js";
     import { elasticOut } from 'svelte/easing';
+    import ProjectCard from '$lib/components/ProjectCard.svelte';
+    import {page} from '$app/state'
+
+    let index = [0, 1];
+    let showcaseProjects = [portfolioData.projects[index[0]], portfolioData.projects[index[1]]];
 
     let visible = $state(false);
 
@@ -46,22 +51,39 @@
 
 
 
-
-{#if visible}
-    <div
-      class="centered"
-      in:spin={{ duration: 8000 }}
-      out:spin={{ duration: 8000 }}
-    >
-        <span>transitions!</span>
-    </div>
-{/if}
+<div class="centered flex-row">
+    <ProjectCard>
+        title={showcaseProjects[0].title}
+        description={showcaseProjects[0].description}
+        image={showcaseProjects[0].image}
+        path={page.url.toString() + '/' + index[0]}
+    </ProjectCard>
+    {#if visible}
+        <div
+            class="centered-spinning"
+            in:spin={{ duration: 8000 }}
+            out:spin={{ duration: 8000 }}
+        >
+            <span>transitions!</span>
+        </div>
+    {/if}
+    <ProjectCard>
+        title={showcaseProjects[0].title}
+        description={showcaseProjects[0].description}
+        image={showcaseProjects[0].image}
+        path={page.url.toString() + '/' + index[0]}
+    </ProjectCard>
+</div>
 
 <style>
     h2 {
         text-align: center;
         font-size: 2em;
         letter-spacing: 15px;
+    }
+    .flex-row{
+        display: flex;
+        flex-direction: row;
     }
 
     .centered {
@@ -70,6 +92,13 @@
         top: 50%;
         transform: translate(-50%, -50%);
     }
+
+    .centered-spinning{
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
     span {
         position: absolute;
         transform: translate(-50%, -50%);
