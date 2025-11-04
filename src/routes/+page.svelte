54580bbbec7b07f913fe95270119a84005d0012f
Jannes <146443595+temporarily-permanent@users.noreply.github.com>
2025-11-04 13:33:35 +0100

    clearly some work is still required

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,6 +41,11 @@
 
     <input type="checkbox" bind:checked={visible} />
 
+    <label class="switch">
+        <input type="checkbox">
+        <span class="slider"></span>
+    </label>
+
 
 
 </p>
@@ -73,7 +78,7 @@
         title={showcaseProjects[1].title}
         description={showcaseProjects[1].description}
         image={showcaseProjects[1].image}
-        path={page.url.toString() + '/' + index[1]}
+        path={page.url.toString() + 'Projects/' + index[1]}
     />
 </div>
 
@@ -86,6 +91,7 @@
     .flex-row{
         display: flex;
         flex-direction: row;
+        gap: 20px;
     }
 
     .centered {
@@ -96,17 +102,76 @@
     }
 
     .centered-spinning{
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
+        position: relative;
+        margin: auto 0;
     }
 
     span {/*
-        position: absolute;*/
-        transform: translate(-50%, -50%);
+        position: absolute;*//*
+        transform: translate(-50%, -50%);*/
         font-size: 4em;
         color: unset;
     }
+
+
+
+
+    /*begin uiverse toggle*/
+
+    /* From Uiverse.io by gharsh11032000 */
+    /* The switch - the box around the slider */
+    .switch {
+        font-size: 17px;
+        position: relative;
+        display: inline-block;
+        width: 3.5em;
+        height: 2em;
+    }
+
+    /* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    /* The slider */
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        inset: 0;
+        background: #9fccfa;
+        border-radius: 50px;
+        transition: all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+
+    .slider:before {
+        position: absolute;
+        content: "";
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 2em;
+        width: 2em;
+        inset: 0;
+        background-color: white;
+        border-radius: 50px;
+        box-shadow: 0 10px 20px rgba(0,0,0,0.4);
+        transition: all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+
+    .switch input:checked + .slider {
+        background: #0974f1;
+    }
+
+    .switch input:focus + .slider {
+        box-shadow: 0 0 1px #0974f1;
+    }
+
+    .switch input:checked + .slider:before {
+        transform: translateX(1.6em);
+    }
+
+    /*end uiverse toggle*/
 
 </style>
